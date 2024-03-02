57022168bd42c3a536e439a431bc306ec668a748
Nitheesh24 <nitheeshthotakura@gmail.com>
2024-03-02 16:15:18 +0530

    datatransformation has completed

--- src/pipelines/training_pipeline.py
+++ src/pipelines/training_pipeline.py
@@ -5,12 +5,15 @@ from src.exception import CustomException
 import pandas as pd
 
 from src.components.data_ingestion import DataIngestion
+from src.components.data_transformation import DataTransformation
 
 data_ingestion_instance = DataIngestion()
 
 try:
     # Call the instance method on the created instance
     train_data_path, test_data_path = data_ingestion_instance.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    train_arr,test_arr,obj_path = data_transformation.initiate_data_transformation(train_data_path,test_data_path)
 except CustomException as e:
     print(f"Error: {e}")
     # Handle the exception gracefully, maybe log it or provide a default value
